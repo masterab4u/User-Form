4a298bc32b99a873e4180c65f93c8b0bb4ea0a01
masterab4u <master.ab4u@gmail.com>
2021-11-08 11:33:45 +0530

    update

---
+++ src/main/java/com/example/singlepageApp/services/DepartmentService.java
@@ -0,0 +1,23 @@
+package com.example.singlepageApp.services;
+
+import java.util.List;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.singlepageApp.models.Department;
+import com.example.singlepageApp.repositories.DepartmentRepository;
+
+@Service
+public class DepartmentService {
+	
+	@Autowired
+	private DepartmentRepository departmentRepository;
+	
+	public List<Department> getAll(){
+		return (List<Department>) departmentRepository.findAll();
+	}
+
+}
